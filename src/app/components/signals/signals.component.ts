1c4f5899101b7abb5fbe7ba5568bcd2495a23457
Wellinton Alexandre <wellinton1067@gmail.com>
2024-01-26 22:49:30 -0300

    Added signals - computed study

--- src/app/components/signals/signals.component.ts
+++ src/app/components/signals/signals.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-signals',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './signals.component.html',
   styleUrl: './signals.component.scss'
 })
@@ -11,11 +12,23 @@ export class SignalsComponent {
   public name = signal('Zed ')
   public lastName = signal('Stephan da Silva Sais')
 
+  public obj = signal({ name: 'Zed', age: 9 })
+
   public fullName = computed(() => {
     return this.name() + this.lastName()
   })
 
-  public update() {
+  // set define o signal para um novo valor absoluto.
+  public set() {
     return this.name.set('Zoe ')
   }
+
+  // upadate define o signal com base no valor atual.
+  public update() {
+    this.obj.update(currentValue => {
+      return {...currentValue, age: 15}
+    })
+  }
+
+  // Mutate define o signal com base no valor atual sem o obj total
 }
